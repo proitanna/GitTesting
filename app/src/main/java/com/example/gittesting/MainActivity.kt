38a82a2eeef019dd31401d294848279535071964
Anna <an.psylove@gmail.com>
2022-08-14 07:56:14 +0300

    feat: println fifth commit

--- app/src/main/java/com/example/gittesting/MainActivity.kt
+++ app/src/main/java/com/example/gittesting/MainActivity.kt
@@ -10,5 +10,6 @@ class MainActivity : AppCompatActivity() {
         println("Hello, first commit")
         println("Student")
         println("Hello from your friend :) ")
+        println("fifth commit")
     }
 }
